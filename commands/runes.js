d9ca60dd611f04c59328ab5f0a65ac5124d544fe
Kertor <70354024+Git-Kertor@users.noreply.github.com>
2022-02-16 21:59:07 +0200

    Create runes.js

---
+++ commands/runes.js
@@ -0,0 +1,35 @@
+const Discord = require("discord.js");
+const { prefix } = require("../config.js");
+
+var fs = require('fs');
+var path = require('path');
+var truePath = path.join(__dirname, '../runes.txt');
+
+var data = fs.readFileSync(truePath);
+splits = data.toString().split("\n");
+
+let commands = {};
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+module.exports = {
+  name: "runes",
+  description: "randomRunes",
+  execute: (message, args) => {   
+    var teamSize = parseInt(args[0]);
+    var str = "";
+    str += "Team One: ";
+    for(let i = 0; i < teamSize; i++) {
+      str += splits[getRandomInt(splits.length)];
+      if(i < teamSize - 1) {str += ", ";}
+    }
+    str += "\nTeam Two: ";
+    for(let i = 0; i < teamSize; i++) {
+      str += splits[getRandomInt(splits.length)];
+      if(i < teamSize - 1) {str += ", ";}
+    }
+    message.channel.send(str);
+  },
+}
