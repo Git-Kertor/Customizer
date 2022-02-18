527d2d975a02cdccc98849dba6c7dd66b167e99a
Kertor <70354024+Git-Kertor@users.noreply.github.com>
2022-02-18 09:10:49 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -8,7 +8,7 @@ module.exports = {
   name: "help",
   description: "commands",
   execute: (message, args) => {
-    message.channel.send("!champion for a random champion\n!runes (x) for random runes\n!teams (x) for random teams");
+    message.channel.send("!champion for a random champion\n!runes (x) for random runes\n!team (x) for random teams");
   },
   setCommands: (newCommands) => {
     commands = newCommands;
